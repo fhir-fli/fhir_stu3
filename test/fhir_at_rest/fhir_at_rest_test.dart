7fd439698d3f398aef172c9f4bc7f8710483fbee
Dokotela <grey.faulkenberry@mayjuun.com>
2024-04-24 09:53:24 +0200

    removed testing

--- test/fhir_at_rest/fhir_at_rest_test.dart
+++
@@ -1,9 +0,0 @@
-import 'fhir_uri_test.dart';
-import 'http_request_tests.dart';
-import 'requests_test.dart';
-
-Future<void> main() async {
-  fhirUriTest();
-  httpRequestTests();
-  await requestsTest();
-}
